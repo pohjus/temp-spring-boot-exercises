88dddd320b583b35c654d7b5bb2c7782e1406d1f
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2022-09-29 10:05:58 +0300

    Dependency injection: exe 1 done

--- dependency-injection/src/main/java/com/example/demo/DemoApplication.java
+++ dependency-injection/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+
+    public DemoApplication() {
+        System.out.println("Demo app created");
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("server ready");
+    }
 }
---
+++ dependency-injection/src/main/java/com/example/demo/HotDog.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HotDog {
+
+    public HotDog() {
+        System.out.println("Hot dog created");
+    }
+}
